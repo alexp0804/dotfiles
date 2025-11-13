49b309ba7db81aa28cb6eddacd865e6542c98dfb
Alexander Peterson <for.alexpeterson@gmail.com>
2025-11-12 18:08:59 -0800

    feat: add lazydev for better nvim config editing

--- nvim/.config/nvim/lua/alex/plugins/blink.lua
+++ nvim/.config/nvim/lua/alex/plugins/blink.lua
@@ -3,10 +3,19 @@ return {
     dependencies = { "rafamadriz/friendly-snippets" },
     version = "1.*",
     opts = {
+        sources = {
+            default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+            providers = {
+                lazydev = {
+                    name = "LazyDev",
+                    module = "lazydev.integrations.blink",
+                    score_offset = 100,
+                },
+            },
+        },
         keymap = { preset = "default" },
         appearance = { nerd_font_variant = "mono" },
         completion = { documentation = { auto_show = true } },
-        sources = { default = { "lsp", "path", "snippets", "buffer" } },
         fuzzy = { implementation = "prefer_rust_with_warning" },
     },
     opts_extend = { "sources.default" },
